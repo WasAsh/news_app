ab638b8fbd2f2630fbfc1fe487d619c702c4d1d6
waseem <wasee.2132254@gmail.com>
2020-01-18 21:54:02 +0300

    commit , simple news app

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData appTheme = ThemeData(
-    primaryColor: Colors.red.shade800 ,
+    primaryColor: Colors.purple ,
 
   ) ;
 }
